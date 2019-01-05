1f230a5d30dda825c2ceb5d224275786b19165d1
antheboets <anthe.boets@gmail.com>
2019-01-05 02:26:59 +0100

    Create EncounterSquad.php

---
+++ Data/EncounterSquad.php
@@ -0,0 +1,14 @@
+<?php
+class EncounterSquad{
+
+    public $sqaud;
+    public $EncounterRate;
+
+    public function __construct($sqaud, $EncounterRate){
+        $this->sqaud = $sqaud;
+        $this->EncounterRate = $EncounterRate;
+    }
+
+
+}
+?>
